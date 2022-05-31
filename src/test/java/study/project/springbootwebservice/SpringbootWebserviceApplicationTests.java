3397edd3313cebec189181ecd0eb944701ac6305
jiyoon <vywns9978@gmail.com>
2022-06-01 01:53:58 +0900

    05/31 Spring project

---
+++ src/test/java/study/project/springbootwebservice/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.project.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
