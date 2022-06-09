73c39926e9d2e42b7683eeaa18173f32c352ce5b
jiyoon <vywns9978@gmail.com>
2022-06-09 12:54:28 +0900

    ch5.3 progress (06/09)

---
+++ src/main/java/com/project/springboot/domain/user/Role.java
@@ -0,0 +1,20 @@
+package com.project.springboot.domain.user;
+
+//Role 클래스 : 각 사용자의 권한을 관리할 Enum클래스
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Role {
+
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    //스프링 시큐리티에서는 권한 코드에 항상 ROLE_이 앞에 있어야만 함.
+
+    private final String key;
+    private final String title;
+
+}
---
+++ src/main/java/com/project/springboot/domain/user/UserRepository.java
@@ -0,0 +1,19 @@
+package com.project.springboot.domain.user;
+
+//UserRepository = User의 CRUD를 책임짐.
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public class UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    /*
+        1. findByEmail
+            - 소셜 로그인으로 반환되는 값 중 email을 통해 이미 생성된 사용자인지 처음 가입하는 사용자인지
+              판단하기 위한 메소드.
+     */
+}
